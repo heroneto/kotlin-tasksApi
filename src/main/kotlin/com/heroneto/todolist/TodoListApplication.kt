2dfd4002d841ba72917f50ffa897cb5430de6712
Heron Eto <hhqe@hotmail.com>
2024-02-21 06:57:41 -0300

    renomeia application

--- src/main/kotlin/com/heroneto/todolist/TodoListApplication.kt
+++ src/main/kotlin/com/heroneto/todolist/TodoListApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TasksApplication
+class TodoListApplication
 
 fun main(args: Array<String>) {
-	runApplication<TasksApplication>(*args)
+	runApplication<TodoListApplication>(*args)
 }
