9272e6cb307c3311ac383bd740b6f8eb42527889
Heron Eto <hhqe@hotmail.com>
2024-02-26 20:10:58 -0300

    adiciona conexão com banco e docker compose mongodb

--- src/main/kotlin/com/heroneto/todolist/TodoListApplication.kt
+++ src/main/kotlin/com/heroneto/todolist/TodoListApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class TodoListApplication
 
-fun main(args: Array<String>) {
-	runApplication<TodoListApplication>(*args)
+
+suspend fun main(args: Array<String>) {
+	setupConnection()
+//	runApplication<TodoListApplication>(*args)
 }
